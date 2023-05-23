6eecb768dc884e85c8b3944a287ae2aa6c233083
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-05-23 23:01:08 +0530

    Added solution - LeetHub

--- Inorder Successor in BST - GFG/inorder-successor-in-bst.java
+++ Inorder Successor in BST - GFG/inorder-successor-in-bst.java
@@ -128,14 +128,13 @@ class Solution
     // returns the inorder successor of the Node x in BST (rooted at 'root')
 	public Node inorderSuccessor(Node root,Node x)
          {
-          
-          Node successor= null;
+          //add code here.
+           Node successor=null;
           while(root!=null)
           {
               if(x.data>=root.data)
               {
                   root=root.right;
-                  
               }
               else
               {
@@ -144,7 +143,5 @@ class Solution
               }
           }
           return successor;
-          
-          
          }
 }
