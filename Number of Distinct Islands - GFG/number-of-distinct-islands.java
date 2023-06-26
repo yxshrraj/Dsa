f2888cfd7f6fa71c0b8213bc50efc6a7c7b01a20
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-06-26 12:49:38 +0530

    Added solution - LeetHub

---
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] grid = new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+
+                for (int j = 0; j < m; j++) {
+                    grid[i][j] = sc.nextInt();
+                }
+            }
+
+            Solution ob = new Solution();
+            int ans = ob.countDistinctIslands(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void dfs(int row, int col, int[][] vis, int[][] grid,ArrayList<String> arr,int row0 ,int col0) {
+       
+        vis[row][col] = 1;
+        arr.add(toString(row-row0 ,col-col0));
+          int n= grid.length;
+          int m= grid[0].length;
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + dr[i];
+            int ncol = col + dc[i];
+
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
+                dfs(nrow, ncol, vis, grid,arr,row0,col0);
+            }
+        }
+    }
+     String toString(int r, int c) {
+    return Integer.toString(r) + " " + Integer.toString(c);
+  }
+    int countDistinctIslands(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int[][] vis = new int[n][m];
+        HashSet<ArrayList<String>> st =new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && vis[i][j] == 0) {
+                    ArrayList<String> arr = new ArrayList<>();
+                    dfs(i, j, vis, grid,arr,i,j);
+                    st.add(arr);
+                }
+            }
+        }
+        
+        return st.size();
+    }
+}
