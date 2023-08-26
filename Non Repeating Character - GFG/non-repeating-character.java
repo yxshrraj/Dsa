4178c41404b8fb75d3619e17217eb265128a2986
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-08-26 17:19:00 +0530

    Added solution - LeetHub

--- Non Repeating Character - GFG/non-repeating-character.java
+++ Non Repeating Character - GFG/non-repeating-character.java
@@ -37,29 +37,21 @@ class Solution
     static char nonrepeatingCharacter(String S)
     {
         //Your code here
-        HashMap<Character,Integer> mp =new HashMap<Character,Integer>();
-        char x ='$';
+        char x='$';
+        char [] ans= new  char[26];
         for(int i=0 ;i<S.length();i++)
         {
             char c= S.charAt(i);
-            if(mp.containsKey(c))
-            {
-               mp.put(c, mp.getOrDefault(c, 0) + 1);
- 
-            }
-            else
-            {
-                mp.put(c,1);
-            }
+            ans[c-'a']++;
         }
-        for (int i = 0; i < S.length(); i++) {
-            char c = S.charAt(i);
-            if (mp.get(c) == 1) {  // Check if count is 1
-                x = c;
-                break;
-            }
+        for(int i=0 ;i<S.length();i++)
+        {
+            char c=S.charAt(i);
+            if(ans[c-'a']==1)
+             return c;
         }
-        return x;
+        return '$';
     }
+    
 }
 
