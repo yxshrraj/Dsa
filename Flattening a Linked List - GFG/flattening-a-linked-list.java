5f5e1b1f68ab4897526de215ea369b6371b52b70
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-11-09 10:05:23 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.java
+++ Flattening a Linked List - GFG/flattening-a-linked-list.java
@@ -119,47 +119,48 @@ class Node
     the flattened linked list. */
 class GfG
 {
-    Node merge(Node l1 ,  Node l2)
+    Node merge(Node l1 , Node l2)
     {
        Node dummy = new Node(0);
-        Node temp= dummy;
-        
+        Node temp=dummy;
         while(l1!=null && l2!=null)
         {
             if(l1.data<=l2.data)
             {
-                temp.bottom = l1;
-                l1=l1.bottom ;
+                temp.bottom=l1;
+                l1=l1.bottom;
                 temp=temp.bottom;
             }
             else
-            {
-                temp.bottom=l2;
-                l2=l2.bottom;
-                temp=temp.bottom;
-            }
+             {
+                 temp.bottom=l2;
+                 l2=l2.bottom;
+                 temp=temp.bottom;
+             }
         }
         while(l1!=null)
         {
-          temp.bottom = l1;
-                l1=l1.bottom ;
-                temp=temp.bottom;  
+             temp.bottom=l1;
+                l1=l1.bottom;
+                temp=temp.bottom;
         }
         while(l2!=null)
-         {
-               temp.bottom=l2;
-                l2=l2.bottom;
-                temp=temp.bottom;
-         }
+        {
+            temp.bottom=l2;
+                 l2=l2.bottom;
+                 temp=temp.bottom;
+        }
         return dummy.bottom;
     }
     Node flatten(Node root)
     {
-        if(root==null || root.next==null) return root;
-        
-        root.next= flatten(root.next);
-        
-        root= merge(root,root.next);
-        return root;
+	// Your code here
+	   if(root==null || root.next==null)
+	   {
+	       return root;
+	   }
+	   root.next=flatten(root.next);
+	   root=merge(root,root.next);
+	   return root;
     }
 }
